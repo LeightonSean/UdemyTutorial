13121c71d753ea3e9e0fb18fed021ec9d5dc05b9
Sean Leighton <seanleighton24@gmail.com>
2016-02-14 17:00:47 -0500

    Map Demo

---
+++ Map Demo/Map Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map Demo
+//
+//  Created by Sean Leighton on 2/14/16.
+//  Copyright © 2016 Sean Leighton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
